ac76df97c84744d515a5b794f4c26cffb4059193
Emir Şahin <64266261+Milkakiina@users.noreply.github.com>
2021-10-05 15:47:26 +0300

    Add files via upload

--- MovieDL/Interfaces/IActorRepo.cs
+++ MovieDL/Interfaces/IActorRepo.cs
@@ -6,18 +6,11 @@ using System.Threading.Tasks;
 
 namespace MovieDL.Interfaces
 {
-    public interface IMovieRepo
+    public interface IActorRepo
     {
-        object dbc { get; }
-
-        List<Movies> getAllMovies();
-        void addMovie(Movies m);
         List<Actors> getActors(Guid movieID);
         public List<Actors> getAllActors();
-        public List<MovieActors> getAllMovieActors();
-
         void addActor(Actors actor);
-        void addActorToMovie(Guid actorID, Guid movieID);
 
     }
 }
---
+++ MovieDL/Interfaces/ILoginRepo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieDL.Interfaces
+{
+    public interface ILoginRepo
+    {
+        public bool isAdmin(string username, string password);
+        public bool isUser(string username, string password);
+        public void createUser(string userName, string password, bool isAdmin);
+    }
+}
